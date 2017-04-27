2aec603d77cdaf9b9fcf95555d6f7b71c9070422
Fahima Zulfath <fahimazulfath.a@gmail.com>
2017-04-27 22:17:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ var config = {
   database: 'fahimazulfath',
   host: 'db.imad.hasura-app.io',
   port: '5432',
-  password: 'db-fahimazulfath-18473',
-  //  password: process.env.DB_PASSWORD,
-
+  password: process.env.DB_PASSWORD,
 };
 
 var app = express();
